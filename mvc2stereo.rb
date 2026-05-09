91f86877c451305566267d04ddaaf80fc97bb4c1
stereo3d <uwe2@gmx.net>
2026-05-10 00:35:15 +0900

    Update SHA256 for macOS ARM64 release

--- mvc2stereo.rb
+++ mvc2stereo.rb
@@ -5,7 +5,7 @@ class Mvc2stereo < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/stereo3d/mvc2stereo-releases/releases/download/v0.1.0/mvc2stereo-0.1.0-macos-arm64.tar.gz"
-    sha256 "PASTE_SHA256_HERE"
+    sha256 "7ba648f8bc9e353b9b8fd815ce3af0a5cf61b2d3f5d4152fd505916fe001662d"
   else
     odie "mvc2stereo is currently distributed only for Apple Silicon Macs"
   end
